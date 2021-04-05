7373de39ad188d9d995e3d1097446ea35873e862
Kartik Derasari <derasarikartik@gmail.com>
2021-04-05 16:43:34 +0530

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar absolute dense color="white" class="px-4">
-    <v-app-bar-title> GitHub User Directory </v-app-bar-title>
+    <h3 class="secondary--text text-no-wrap pt-1">GitHub User Directory</h3>
     <v-tabs right>
       <v-tab to="/">Home</v-tab>
       <v-tab to="/about">About</v-tab>
